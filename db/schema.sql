f94acceb6c6400e5e9772cc9781b0a710afdc1bd
Heath Ward <heathw24@outlook.com>
2021-02-06 11:54:11 -0500

    set up db folder and schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE books_db;
+
+USE books_db;
+
+CREATE TABLE books
+(
+  title VARCHAR(255) NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  readerID INTEGER NOT NULL,
+  haveRead BOOLEAN DEFAULT false,
+  id INT AUTO_INCREMENT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE readers
+(
+    id INT AUTO_INCREMENT NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    passwords VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
